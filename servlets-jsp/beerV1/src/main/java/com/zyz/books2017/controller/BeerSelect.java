957e57cc8bb744a42deb3f4f2a8654e56daf78fd
zz0116 <zyz9255@gmail.com>
2017-02-18 13:33:30 +0800

    learn more

--- servlets-jsp/beerV1/src/main/java/com/zyz/books2017/controller/BeerSelect.java
+++ servlets-jsp/beerV1/src/main/java/com/zyz/books2017/controller/BeerSelect.java
@@ -1,11 +1,14 @@
 package com.zyz.books2017.controller;
 
+import com.zyz.books2017.model.BeerExpert;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.List;
 
 /**
  * Created by ZhangYuanzhuo on 2017/2/17.
@@ -16,11 +19,27 @@ public class BeerSelect extends HttpServlet {
 						  HttpServletResponse response)
 			throws ServletException, IOException {
 
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		out.println("Beer Selection Advice<br>");
+//		response.setContentType("text/html");
+//		PrintWriter out = response.getWriter();
+//		out.println("Beer Selection Advice<br>");
+
 		String c = request.getParameter("color");
-		out.println("<br>Got beer color " + c);
+
+//		out.println("<br>Got beer color " + c);
+
+		BeerExpert be = new BeerExpert();
+		List result = be.getBrands(c);
+
+//		Iterator it = result.iterator();
+//		while (it.hasNext()) {
+//			out.print("<br>try: " + it.next());
+//		}
+
+		request.setAttribute("styles", result);
+
+		RequestDispatcher view = request.getRequestDispatcher("result.jsp");
+
+		view.forward(request, response);
 	}
 
 }
--- servlets-jsp/beerV1/src/main/java/com/zyz/books2017/model/Model.java
+++
@@ -1,7 +0,0 @@
-package com.zyz.books2017.model;
-
-/**
- * Created by ZhangYuanzhuo on 2017/2/17.
- */
-public class Model {
-}
